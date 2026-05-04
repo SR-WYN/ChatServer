efa62a6e34e0783cfd8d2a9ae887347643769efb
wangyanan <2368495219@qq.com>
2026-05-04 14:18:59 +0800

    AI休整用户数据相关字段

--- data.h
+++ data.h
@@ -4,7 +4,8 @@
 struct UserInfo
 {
     UserInfo()
-        : name(""), pwd(""), uid(0), email(""), nick(""), desc(""), sex(0), icon(""), back("")
+        : name(""), pwd(""), uid(0), email(""), nick(""), desc(""), sex(0), icon(""), back(""),
+          alias_name("")
     {
     }
 
@@ -17,13 +18,17 @@ struct UserInfo
     int sex;
     std::string icon;
     std::string back;
+    /** 仅好友列表合并行使用：self 对 friend 的备注（friend 表）；从 user 表加载时应保持为空 */
+    std::string alias_name;
 };
 
 struct ApplyInfo
 {
     ApplyInfo(int uid, const std::string &name, const std::string &desc, const std::string &icon,
-              const std::string &nick, int sex, int status)
-        : _uid(uid), _name(name), _desc(desc), _icon(icon), _nick(nick), _sex(sex), _status(status)
+              const std::string &nick, int sex, int status,
+              const std::string &alias_name = std::string())
+        : _uid(uid), _name(name), _desc(desc), _icon(icon), _nick(nick), _sex(sex), _status(status),
+          alias_name(alias_name)
     {
     }
 
@@ -34,4 +39,6 @@ struct ApplyInfo
     std::string _nick;
     int _sex;
     int _status;
+    /** friend_apply.alias_name：申请人对被申请人的备注 */
+    std::string alias_name;
 };
